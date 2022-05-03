b1f521ecc4dee1f6892942623e373d73c00e9ee4
Hoidberg <hoidberg@hotmail.com>
2022-05-02 19:41:55 -0500

    HOUSEKEEPING

--- sort.py
+++ sort.py
@@ -1,41 +1,5 @@
 delta = [None] * 4
 
-def initArgs(func):
-    @wraps(func)
-    def functionWrapper(*args, **kw):
-
-        argNames, defaults = inspect.getargspec(func)[::3]
-
-        for k in kw:
-            for i in argNames:
-                if k != i and ('_' + k) == i:
-                    kw['_' + k] = kw[k]
-                    del kw[k]
-
-        argValues = list(args) + [
-            kw[y] if y in kw else defaults[x]
-            for x,y in enumerate(argNames[len(args):])
-        ]
-
-        oldGlobals = {}
-        for n,v in zip(argNames, argValues):
-
-            try:
-                oldGlobals[n] = globals()[n]
-            except:
-                oldGlobals[n] = None
-
-            if not n.startswith('_') or n in kw:
-                globals()[n] = v
-            else:
-                globals()[n] = kw[n] = eval(v)
-
-        for o,v in oldGlobals.items(): globals()[o] = v
-
-        return func(*args, **kw)
-
-    return functionWrapper
-
 def make_delta(N: int):
     power = 1
     i = 0
@@ -48,7 +12,8 @@ def make_delta(N: int):
             break
         i += 1
 
-def binarysearch(el: int, a: list, k: int):
+def binarysearch(el: int, a: list, k=None):
+    k = k if k is not None else len(a)
     make_delta(k)
     i = delta[0] - 1
     d = 0
@@ -69,4 +34,4 @@ def binarysearch(el: int, a: list, k: int):
                 return -1
 
 rand = [1, 3, 5, 8, 9, 11]
-print(binarysearch(9, rand, 5))
+print(binarysearch(11, rand))
